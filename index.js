670d0d17ec4fb667d3da8df0702c2ef769ee3d24
nibanerjee <Nilgitrepo@2018>
2018-08-22 16:52:12 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// main app
+import App from './containers/App';
+
+ReactDOM.render(<App />, document.getElementById('app'))
