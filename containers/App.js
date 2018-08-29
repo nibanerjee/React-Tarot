036d08eb1915cea8e400cdbb2f0a531cad62f59c
nibanerjee <Nilgitrepo@2018>
2018-08-29 14:52:46 +0530

    cards css material design

--- containers/App.js
+++ containers/App.js
@@ -1,16 +1,62 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import '../styles/app.less';
+import $ from 'jquery';
 export default class App extends Component {
     render () {
         return (
-            <ul>
-                <li><Link to="/spread1">The “True Love” Spread</Link></li>
-                <li><Link to="/spread2">The “Success” Spread</Link></li>
-                <li><Link to="/spread3">The “Celtic Cross” Spread</Link></li>
-                <li><Link to="/spread4">The “Spiritual Guidance” Spread</Link></li>
-                <li><Link to="/spread5">The “Career Path” Spread</Link></li>
-                <li><Link to="/spread6">The “Three Card” Spread</Link></li>
-            </ul>
+            <div class="container tarot-app-container">
+                <div class="row">
+                    <div class="col-sm card-col">
+                        <div class="card" onClick={this.flipCards}>
+                            <div class="front">The “True Love” Spread</div>
+                            <div class="back">
+                                <div class="short-desc">
+                                    <span>It should come as no surprise that this is one of the spreads on this list. Everyone likes to know where their love lives are going, what will happen to their romantic relationships, and if they will be happy. This spread is very useful to evaluate your physical, emotional, mental and spiritual connections with your partner. <Link to="/spread1">Click here for details</Link></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm card-col">
+                        <div class="card" onClick={this.flipCards}>
+                            <div class="front">The “Success” Spread</div>
+                            <div class="back"><Link to="/spread2">Details</Link></div>
+                        </div>
+                    </div>
+                    <div class="col-sm card-col">
+                        <div class="card" onClick={this.flipCards}>
+                            <div class="front">The “Celtic Cross” Spread</div>
+                            <div class="back"><Link to="/spread3">Details</Link></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm card-col">
+                        <div class="card" onClick={this.flipCards}>
+                            <div class="front">The “Spiritual Guidance” Spread</div>
+                            <div class="back"><Link to="/spread4">Details</Link></div>
+                        </div>
+                    </div>
+                    <div class="col-sm card-col">
+                        <div class="card" onClick={this.flipCards}>
+                            <div class="front">The “Career Path” Spread</div>
+                            <div class="back">
+                                <Link to="/spread5">Details</Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm card-col">
+                        <div class="card" onClick={this.flipCards}>
+                            <div class="front">The “Three Card” Spread</div>
+                            <div class="back"><Link to="/spread6">Details</Link></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
+    }
+    flipCards = (event) => {
+        event.preventDefault();
+        $(event.target.parentNode).toggleClass('flipped');
     }
 }
