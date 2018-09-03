6964d0d3e1c29e5a345738832ebf08116d6ad465
nibanerjee <Nilgitrepo@2018>
2018-09-03 15:50:29 +0530

    spread1 completed

--- containers/App.js
+++ containers/App.js
@@ -8,9 +8,13 @@ export default class App extends Component {
             <div class="container tarot-app-container">
                 <div class="row">
                     <div class="col-sm card-col">
-                        <div class="card" onClick={this.flipCards}>
-                            <div class="front">The “True Love” Spread</div>
+                        <div class="card">
+                            <div class="front">
+                                <span>The “True Love” Spread</span>
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                            </div>
                             <div class="back">
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
                                 <div class="short-desc">
                                     <span>It should come as no surprise that this is one of the spreads on this list. Everyone likes to know where their love lives are going, what will happen to their romantic relationships, and if they will be happy. This spread is very useful to evaluate your physical, emotional, mental and spiritual connections with your partner. <Link to="/spread1">Click here for details</Link></span>
                                 </div>
@@ -18,37 +22,75 @@ export default class App extends Component {
                         </div>
                     </div>
                     <div class="col-sm card-col">
-                        <div class="card" onClick={this.flipCards}>
-                            <div class="front">The “Success” Spread</div>
-                            <div class="back"><Link to="/spread2">Details</Link></div>
+                        <div class="card">
+                            <div class="front">
+                                <span>The “Success” Spread</span>
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                            </div>
+                            <div class="back">
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                                <div class="short-desc">
+                                    <span>The success spread an excellent situational spread to use when you are facing an obstacle or challenge and do not know how to approach it. This spread helps you to better understand the true nature of the obstacle you face, as well as helping you to identify what skills and resources you have at your disposal to not just face, but overcome these obstacles! <Link to="/spread2">Click here for details</Link></span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="col-sm card-col">
-                        <div class="card" onClick={this.flipCards}>
-                            <div class="front">The “Celtic Cross” Spread</div>
-                            <div class="back"><Link to="/spread3">Details</Link></div>
+                        <div class="card">
+                            <div class="front">
+                                <span>The “Celtic Cross” Spread</span>
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                            </div>
+                            <div class="back">
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                                <div class="short-desc">
+                                    <span>Despite it’s complexity, the “Celtic Cross” is a spread that has been in used for many ages. And in it’s complexity, lays its charm. I find this spread to be the most helpful in complicated situations, because it’s versatile and it’s positions deliver vast amount of information that can be read in several ways depending on the combinations. It points to issues that are of the client’s own making, but also shows what outside influences are at play. <Link to="/spread3">Click here for details</Link></span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-sm card-col">
-                        <div class="card" onClick={this.flipCards}>
-                            <div class="front">The “Spiritual Guidance” Spread</div>
-                            <div class="back"><Link to="/spread4">Details</Link></div>
-                        </div>
-                    </div>
-                    <div class="col-sm card-col">
-                        <div class="card" onClick={this.flipCards}>
-                            <div class="front">The “Career Path” Spread</div>
+                        <div class="card">
+                            <div class="front">
+                                <span>The “Spiritual Guidance” Spread</span>
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                            </div>
                             <div class="back">
-                                <Link to="/spread5">Details</Link>
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                                <div class="short-desc">
+                                    <span>Similar to the Success Spread, the Spiritual Guidance spread is used situationally during times when you are faced with obstacles or challenges of a spiritual nature, usually related to your own growth or development. This spread is designed to give you a broader perspective, and includes information to help you along your spiritual path and important life lessons. <Link to="/spread4">Click here for details</Link></span>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-sm card-col">
-                        <div class="card" onClick={this.flipCards}>
-                            <div class="front">The “Three Card” Spread</div>
-                            <div class="back"><Link to="/spread6">Details</Link></div>
+                        <div class="card">
+                            <div class="front">
+                                <span>The “Career Path” Spread</span>
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                            </div>
+                            <div class="back">
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                                <div class="short-desc">
+                                    <span>If you are an employee who feels like you have remained stagnant, stuck in a rut for years, vying to be promoted, but to no gain, this spread is for you.It is a great spread that will help you to understand any obstacles you may be facing in your professional life, and suggeststrategies to on how to deal with these issue. <Link to="/spread5">Click here for details</Link></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm card-col">
+                        <div class="card">
+                            <div class="front">
+                                <span>The “Three Card” Spread</span>
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                            </div>
+                            <div class="back">
+                                <div class="flip-icon-holder" onClick={this.flipCards}></div>
+                                <div class="short-desc">
+                                    <span>This spread is one the quickest way to get answers to any questions you may have in your life, and it can even provide you with insight and guidance if you are simply feel lost and confused.It is by far one of the most dynamic spreads, and is incredibly effective at highlighting the energy of your past, present and future. <Link to="/spread6">Click here for details</Link></span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -57,6 +99,6 @@ export default class App extends Component {
     }
     flipCards = (event) => {
         event.preventDefault();
-        $(event.target.parentNode).toggleClass('flipped');
+        $(event.target).parents(".card").toggleClass('flipped');
     }
 }
--- containers/Spread1.js
+++ containers/Spread1.js
@@ -1,9 +1,57 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
+import '../styles/spread1.less';
 export default class Spread1 extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            showCardFormation : false
+        };
+        this.makeCardFormation = this.makeCardFormation.bind(this);
+    }
+    makeCardFormation(){
+        this.setState({
+            showCardFormation:true
+        })
+    }
     render () {
         return (
-            <p>Spread 1</p>
+            <div class="container spread1-container">
+                    <div class="row shuffle-spread-row">
+                        <div class="spread-container col-sm spread-col">
+                            <div class="row">
+                                <div class="col-sm-4 shuffled-card">
+                                    <span class="card-no">1</span>
+                                    <span class="card-desc">This card represents you. It signifies what you currently feel about your relationship, your approach, and your outlook.</span>
+                                </div>
+                                <div class="col-sm-4 shuffled-card">
+                                    <span class="card-no">2</span>
+                                    <span class="card-desc">The second card represent your partner. It also represents his current emotions towards you, his attitude, and expectations about your relationship.</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm shuffled-card">
+                                    <span class="card-no">3</span>
+                                    <span class="card-desc">This is a connection card. What are the characteristics that you have in common binding you together?</span>
+                                </div>
+                                <div class="col-sm shuffled-card">
+                                    <span class="card-no">4</span>
+                                    <span class="card-desc">This indicates the strength of your relationship. What are the qualities that keep your relationship?</span>
+                                </div>
+                                <div class="col-sm shuffled-card">
+                                    <span class="card-no">5</span>
+                                    <span class="card-desc">This denotes the weakness of your relationship. What are the things you both need to improve?</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-4 shuffled-card">
+                                    <span class="card-no">6</span>
+                                    <span class="card-desc">This final card is your true love card. It interprets what needs to be addressed. Is your relationship going to be successful? If there is a threat, what must be done?</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
         );
     }
 }
